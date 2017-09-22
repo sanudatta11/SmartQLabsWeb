779353aa03618bf242b1e87f4a6baff6194a757c
Soumyajit Dutta <sanudatta11@users.noreply.github.com>
2017-09-22 21:59:36 +0530

    Update mysql_login.php

--- confidential/mysql_login.php
+++ confidential/mysql_login.php
@@ -7,12 +7,10 @@
  */
 
 //Production
-
 $host = "qit.co8zric1dcsi.us-east-1.rds.amazonaws.com:3306";
 $dbname = "qit";
 $username_db = "root";
 $password_db = "mysqlmysql";
-
 
 //Testing
 //$host = "127.0.0.1";
