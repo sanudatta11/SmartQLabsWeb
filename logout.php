0e766f4e9787c3a1a2d8a7ab8b409ef14110e33e
Soumyajit Dutta <sanudatta11@gmail.com>
2017-09-24 05:09:43 +0530

    Added Logout for aDmin

---
+++ logout.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 22/09/17
+ * Time: 9:51 AM
+ */
+require_once $_SERVER['DOCUMENT_ROOT'] . '/confidential/connector.php';
+session_start();
+$email = $_SESSION['email'];
+if (isset($email) && !empty($email)) {
+    $obj = $mysql_conn->prepare("UPDATE " . $dbname . ".login SET sessionid=NULL WHERE email = :email");
+    $obj->bindParam(':email', $email);
+    $obj->execute();
+}
+session_destroy();
+$_SESSION = array();
+header('Location: /main/');
+die();
