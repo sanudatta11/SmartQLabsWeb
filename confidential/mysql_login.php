a0efa47e5cb0b8bacc877895ccfcc894eadc050c
Soumyajit Dutta <sanudatta11@users.noreply.github.com>
2018-02-17 13:20:56 +0530

    Update mysql_login.php

--- confidential/mysql_login.php
+++ confidential/mysql_login.php
@@ -5,17 +5,17 @@
  * Date: 22/09/17
  * Time: 1:56 AM
  */
-
+/*
 //Production
 $host = "localhost";
 $dbname = "id4100767_smartqlabs";
 $username_db = "id4100767_smartqlabs";
 $password_db = "mysqlmysql";
-
+*/
 //Testing
-/*
-$host = "127.0.0.1";
+
+$host = "smartqlabs.co8zric1dcsi.us-east-1.rds.amazonaws.com";
 $dbname = "qit";
 $username_db = "root";
 $password_db = "mysql";
-*/
+
