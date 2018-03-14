7b7696b1b247a95db90b843daa8abfaae378751e
Soumyajit Dutta <sanudatta11@gmail.com>
2018-03-14 11:35:34 +0530

    Changed DB Parameters RDS

--- confidential/mysql_login.php
+++ confidential/mysql_login.php
@@ -6,17 +6,15 @@
  * Time: 1:56 AM
  */
 
-//Production
-$host = "localhost";
-$dbname = "id4100767_smartqlabs";
-$username_db = "id4100767_smartqlabs";
-$password_db = "mysqlmysql";
-
 //Testing
-/*
+//$host = "localhost";
+//$dbname = "id4100767_smartqlabs";
+//$username_db = "id4100767_smartqlabs";
+//$password_db = "mysqlmysql";
+
+//Production
+
 $host = "smartqlabs.co8zric1dcsi.us-east-1.rds.amazonaws.com";
 $dbname = "qit";
 $username_db = "root";
-$password_db = "mysql";
-
-*/
+$password_db = "mysqlmysql";
